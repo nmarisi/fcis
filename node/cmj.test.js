b692cd852418ad70589518ad8158fb43e5917e69
Edward Wong <edward.yueshungwong@iongroup.com>
2022-07-05 20:06:58 +0100

    Extract fetchPosts function and add test coverage that it handles HTTP errors

--- node/cmj.test.js
+++ node/cmj.test.js
@@ -1,4 +1,3 @@
-
 const axios = require("axios");
 const MockAdapter = require("axios-mock-adapter");
 const mock = new MockAdapter(axios);
@@ -6,8 +5,22 @@ const fetchPosts = require('./cms').fetchPosts
 
 it('handles fetching empty posts', async () => {
     mock.onGet("http://jsonplaceholder.typicode.com/posts").reply(() => {
-      return [200, []]
+        return [200, []]
     })
     const data = await fetchPosts()
     expect(data).toEqual([])
+});
+
+it('handles failure to fetch data with error response', async () => {
+    mock.onGet("http://jsonplaceholder.typicode.com/posts").reply(() => {
+        return [403, null]
+    })
+
+    try {
+        await fetchPosts()
+    } catch (err) {
+        expect(err).toEqual("error");
+        return
+    }
+    throw new Error("expected an error")
 });
--- node/cms.js
+++ node/cms.js
@@ -4,46 +4,53 @@ const axios = require('axios').default
 const CMS_URL = 'http://jsonplaceholder.typicode.com/posts'
 
 module.exports = {
-  fetchPosts:  async function() {
-    const res = await axios.get(CMS_URL)
-    console.log('Getting CMS data')
-    if (res.status !== 200) {
-      console.log('Error occurred, status =', res.status)
-      return undefined;
-    }
-    return res.data;
-  },
-
-  main: async function(context) {
-    try {
-      let postData = await this.fetchPosts();
-
-      let today = new Date()
-      today = today.toISOString().slice(0, 10)
-      let { summary } = produceSummary(postData)
-      fs.writeFileSync('cms-' + today + '.json', JSON.stringify(summary))
-      console.log('Wrote CMS report')
-      return Promise.resolve(true)
-
-    } catch (err) {
-      console.log('An error occurred getting the post data', err)
-      return Promise.reject('error')
-    }
-
-    function produceSummary(data) {
-      console.log('CMS data has: ', data.length, 'records')
-      let summary = { posts: 0, users: 0, posts_per_user: 0 }
-      let users = []
-      for (let i = 0; i < data.length; i++) {
-        // console.log('processing record', i, 'for user', res.data[i].userId)
-        summary.posts++
-        if (!users.includes(data[i].userId)) {
-          users.push(data[i].userId)
+    fetchPosts: async function () {
+        let res;
+        try {
+            res = await axios.get(CMS_URL);
+        } catch (err) {
+            console.log('An error occurred getting the post data', err)
+            return Promise.reject('error')
         }
-      }
-      summary.users = users.length
-      summary.mean_posts_per_user = Math.round(summary.posts / summary.users)
-      return  { summary }
+
+        console.log('Getting CMS data')
+        if (res.status !== 200) {
+            console.log('Error occurred, status =', res.status)
+            return undefined;
+        }
+        return res.data;
+    },
+
+    main: async function (context) {
+        try {
+            let postData = await this.fetchPosts();
+
+            let today = new Date()
+            today = today.toISOString().slice(0, 10)
+            let {summary} = produceSummary(postData)
+            fs.writeFileSync('cms-' + today + '.json', JSON.stringify(summary))
+            console.log('Wrote CMS report')
+            return Promise.resolve(true)
+
+        } catch (err) {
+            console.log('An error occurred getting the post data', err)
+            return Promise.reject('error')
+        }
+
+        function produceSummary(data) {
+            console.log('CMS data has: ', data.length, 'records')
+            let summary = {posts: 0, users: 0, posts_per_user: 0}
+            let users = []
+            for (let i = 0; i < data.length; i++) {
+                // console.log('processing record', i, 'for user', res.data[i].userId)
+                summary.posts++
+                if (!users.includes(data[i].userId)) {
+                    users.push(data[i].userId)
+                }
+            }
+            summary.users = users.length
+            summary.mean_posts_per_user = Math.round(summary.posts / summary.users)
+            return {summary}
+        }
     }
-  }
 }
